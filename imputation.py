1edeb1f09a31e9cea8e8cedcef2f1c9342812943
phet1999 <u3009646@connect.hku.hk>
2024-05-27 13:27:00 +0800

    updata_imputation

---
+++ imputation.py
@@ -0,0 +1,160 @@
+import torch
+import torch.nn as nn
+import pandas as pd
+import random
+import numpy as np
+import json
+from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt
+import os
+from sklearn.metrics import mean_squared_error as MSE
+from sklearn.metrics import mean_absolute_error as MAE
+from pypots.imputation import SAITS,BRITS,GPVAE,MRNN,USGAN
+TF_ENABLE_ONEDNN_OPTS=0
+import argparse
+from utils import *
+
+def random_subarray(array, period=24):
+    array1 = np.copy(array)
+    array2 = np.copy(array)
+    length = len(array)
+    num_to_select = int(length * 0.4)
+    selected_length = 0
+
+    replaced_indices = []  # 添加一个列表来存储替换的索引
+    replaced_intervals = []  # 添加一个列表来存储替换的区间
+
+    while selected_length < num_to_select:
+        period_lengths = [period // 10, period // 4, period // 5, period // 2, period, period * 2, period * 4, period * 5]
+        subarray_length = random.choice(period_lengths)
+
+        if subarray_length + selected_length > num_to_select:
+            subarray_length = num_to_select - selected_length
+
+        start_idx = random.randint(0, length - subarray_length)
+        end_idx = start_idx + subarray_length - 1
+
+        # 检查所选区间是否与现有区间重叠
+        overlapping = False
+        for interval in replaced_intervals:
+            if (start_idx >= interval[0] and start_idx <= interval[1]) or (end_idx >= interval[0] and end_idx <= interval[1]):
+                overlapping = True
+                break
+
+        if overlapping:
+            continue  # 如果重叠，跳过当前迭代
+
+        array1[start_idx:end_idx + 1] = np.mean(array[start_idx:end_idx + 1])
+        array2[start_idx:end_idx + 1] = np.nan
+
+        replaced_indices.extend(range(start_idx, end_idx + 1))  # 将替换的索引添加到列表中
+        replaced_intervals.append((start_idx, end_idx))  # 将替换的区间添加到列表中
+
+        selected_length += subarray_length
+
+    return array1, array2, replaced_indices  # 返回修改后的数组以及替换索引的列表
+def replace_nan_with_array(array1, array2):
+    if array1.shape != array2.shape:
+        raise ValueError("两个数组的形状必须相同。")
+
+    result_array = np.copy(array1)
+    nan_indices = np.isnan(array1)
+    result_array[nan_indices] = array2[nan_indices]
+    
+    return result_array
+
+
+def main(args):
+    setup_seed(0)
+    name = args.name
+    repeat_times = args.repeat_times
+    device = args.device
+    data = pd.read_csv(args.data_path,index_col=0)
+    data.index = pd.to_datetime(data.index)
+    # deine timestep
+    time_1 = pd.Timestamp(args.time_1)
+    time_2 = pd.Timestamp(args.time_2)
+    time_3 = pd.Timestamp(args.time_3)
+    # split the data
+    train = np.array(data.loc[time_1:time_2].iloc[:,-1:]).reshape(-1)
+    val = np.array(data.loc[time_2:time_3].iloc[:,-1:])
+    test = np.array(data.loc[time_3:].iloc[:,-1:])
+
+    train_mean,train_nan,nan_index = random_subarray(train,24)
+    train_mean = train_mean.reshape(-1,1)
+    train_nan = train_nan.reshape(-1,1)
+    scaler = StandardScaler()
+    scaler.fit(train_mean)
+    train_mean = scaler.transform(train_mean)
+    train_nan = scaler.transform(train_nan)
+    val =  scaler.transform(val)
+    test = scaler.transform(test)
+
+    train_data_mean,train_label_mean = generate_data(train_mean)
+    train_data_nan,train_label_nan = generate_data(train_nan)
+    val_data,val_label = generate_data(val)
+    test_data,test_label = generate_data(test)
+    
+
+    create_directory_if_not_exists('./imputated_folder')
+    create_directory_if_not_exists('./imputated_folder/'+name)
+
+    np.save('./imputated_folder/'+name+'/train_data_mean.npy', train_data_mean)
+    np.save('./imputated_folder/'+name+'/train_label_mean.npy', train_label_mean)
+    np.save('./imputated_folder/'+name+'/val_data.npy', val_data)
+    np.save('./imputated_folder/'+name+'/val_label.npy', val_label)
+    np.save('./imputated_folder/'+name+'/test_data.npy', test_data)
+    np.save('./imputated_folder/'+name+'/test_label.npy', test_label)
+
+    im_dataset = {"X": train_label_nan}
+
+    for i in range(repeat_times):
+        setup_seed(i+1)
+        model = SAITS(n_steps=24, n_features=1, n_layers=2, d_model=64, d_ffn=32, n_heads=4, d_k=16, d_v=16, dropout=0.1, epochs=100,patience=10,device = device)
+        model.fit(im_dataset)
+        imputation = model.impute(im_dataset)
+        im_result = replace_nan_with_array(train_label_nan,imputation)
+        np.save('./imputated_folder/'+name+'/train_label_saits_'+str(i+1)+'_.npy',im_result)
+
+        model = BRITS(n_steps=24, n_features=1, rnn_hidden_size=64,epochs=100,patience=10,device = device)
+        model.fit(im_dataset)
+        imputation = model.impute(im_dataset)
+        im_result = replace_nan_with_array(train_label_nan,imputation)
+        np.save('./imputated_folder/'+name+'/train_label_brits_'+str(i+1)+'_.npy',im_result)
+
+        model = GPVAE(n_steps=24, n_features=1, latent_size=64,epochs=100,patience=10,device = device)
+        model.fit(im_dataset)
+        imputation = model.impute(im_dataset)[:,0,:,:]
+        im_result = replace_nan_with_array(train_label_nan,imputation)
+        np.save('./imputated_folder/'+name+'/train_label_gpvae_'+str(i+1)+'_.npy',im_result)
+
+        if name != 'ELE':
+            model = MRNN(n_steps=24, n_features=1, rnn_hidden_size=64,epochs=100,patience=10,device = device)
+            model.fit(im_dataset)
+            imputation = model.impute(im_dataset)
+            im_result = replace_nan_with_array(train_label_nan,imputation)
+            np.save('./imputated_folder/'+name+'/train_label_mrnn_'+str(i+1)+'_.npy',im_result)
+
+        model = USGAN(n_steps=24, n_features=1, rnn_hidden_size=64,epochs=100,patience=10,device = device)
+        model.fit(im_dataset)
+        imputation = model.impute(im_dataset)
+        im_result = replace_nan_with_array(train_label_nan,imputation)
+        np.save('./imputated_folder/'+name+'/train_label_usgan_'+str(i+1)+'_.npy',im_result)
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p","--data_path", help="data path",default='./data/GEF.csv')
+    parser.add_argument("-n","--name", help="name of the data",default='GEF',choices=['GEF','ETTH1','ETTH2','ELE','Traffic','Air'])
+    parser.add_argument("-rt","--repeat_times", type=int,help="times of repeating experiment",default=3)
+    parser.add_argument("-d","--device",help="device to run",default='cuda')
+    args = parser.parse_args()
+    with open("./time_settings.json", "r") as file:
+        time_settings = json.load(file)
+    if args.name in time_settings:
+        args.time_1, args.time_2, args.time_3 = time_settings[args.name]
+    main(args)
+
+
+
---
+++ utils.py
@@ -0,0 +1,166 @@
+import os
+import torch
+import torch.nn as nn
+import pandas as pd
+import random
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt
+from sklearn.metrics import mean_squared_error as MSE
+from sklearn.metrics import mean_absolute_error as MAE
+
+import torch
+from torch.autograd import grad
+from torch.utils.data import DataLoader, TensorDataset, Dataset
+import torch.nn as nn
+import torch.optim as optim
+import numpy as np
+import pandas as pd
+from sklearn.metrics import mean_squared_error as MSE
+from sklearn.metrics import mean_absolute_error as MAE
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import MinMaxScaler
+import random
+from tqdm import tqdm
+from tqdm import trange
+import matplotlib.pyplot as plt
+import seaborn as sns
+from torch.utils.data import DataLoader, Subset
+
+import numpy as np
+from scipy.special import digamma
+from sklearn.neighbors import NearestNeighbors
+from tqdm import tqdm
+from torch.autograd.functional import hessian
+from torch.autograd.functional import jacobian
+
+from functorch import make_functional, jvp, grad, hessian, jacfwd, jacrev
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from statsmodels.tsa.arima.model import ARIMA
+from scipy.interpolate import interp1d
+import statsmodels.api as sm
+from statsmodels.tsa.seasonal import seasonal_decompose
+from sklearn.impute import KNNImputer
+import warnings
+from torch.autograd.functional import jacobian
+import copy
+from scipy.signal import periodogram
+# import torch.autograd.functional as F
+import time
+import torch.nn.functional as FF
+from functorch import make_functional, vmap, vjp, jvp, jacrev
+def generate_data(arr: np.array, window_size = 24, num_labels = 24):
+    data, labels = [], []
+
+    for i in range(len(arr) - window_size - num_labels + 1):
+        data.append(arr[i:i + window_size])
+        labels.append(arr[i + window_size:i + window_size + num_labels])
+
+    data = np.array(data)
+    labels = np.array(labels)
+
+    return data, labels
+
+def setup_seed(seed):
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+    torch.backends.cudnn.deterministic = True
+
+def ntk(model, x_i, y_i, x, y, window_length,loss_fn=nn.MSELoss(),batch_size=512):
+    def fnet_single(params, x):
+        return fnet(params, x.unsqueeze(0)).squeeze(0)
+    def empirical_ntk(fnet_single, params, x2):
+        # # Compute J(x1)
+        # jac1 = vmap(jacrev(fnet_single), (None, 0))(params, x1)
+        # jac1 = [j.flatten(2) for j in jac1]
+        
+        # Compute J(x2)
+        jac2 = vmap(jacrev(fnet_single), (None, 0))(params, x2)
+        jac2 = [j.flatten(2) for j in jac2]
+        
+        # # Compute J(x1) @ J(x2).T
+        # result = torch.stack([torch.einsum('Naf,Mbf->NMab', j1, j2) for j1, j2 in zip(jac1, jac2)])
+        # result = result.sum(0)
+        return torch.cat(jac2,-1)
+    # y_i_pred = model(x_i).reshape(-1)
+    y_pred = model(x).reshape(-1)
+    loss = loss_fn(y_pred, y.reshape(-1))
+
+    grad_theta_y = torch.autograd.grad(loss, model.parameters())
+    grad_theta_y = [g.contiguous() for g in grad_theta_y]
+    grad_theta_y_vector = torch.nn.utils.parameters_to_vector(grad_theta_y).detach()
+    fnet, params = make_functional(model)
+    n_samples = x_i.size(0)
+    ntk_kernel = torch.zeros([n_samples, window_length], device=x.device)
+
+    for batch_start in range(0, n_samples, batch_size):
+        batch_end = min(batch_start + batch_size, n_samples)
+        x_batch = x_i[batch_start:batch_end]
+        jacobian_y_i_pred_batch = empirical_ntk(fnet_single, params, x_batch)
+        ntk_kernel_batch = torch.matmul(jacobian_y_i_pred_batch, grad_theta_y_vector.view(-1,1))
+        batch_size_in = batch_end - batch_start
+        ntk_kernel[batch_start:batch_end] = ntk_kernel_batch.squeeze().reshape(batch_size_in, -1)
+    return ntk_kernel
+
+def my_get_yh_v_product(model: nn.Module, X, y, v, loss_fn=nn.MSELoss()):
+    """
+    Compute the product of the Jacobian matrix of the gradient of the loss function with respect to the predictions (mu),
+    with respect to the target values (y) for the whole training set, and a given vector v.
+    """
+    y = y.reshape(-1)
+    y.requires_grad_(True)
+    pred = model(X).reshape(-1)
+    loss = loss_fn(y, pred)
+
+    # First order gradient with respect to predictions (mu)
+    grad = torch.autograd.grad(outputs=loss, inputs=pred, create_graph=True)
+    grad = torch.cat([x.flatten() for x in grad], dim=0)
+
+    # Compute the product Jv
+    # Jv = torch.zeros_like(y).to(X.device)
+    # for i, g in enumerate(grad):
+    #     Jv += g * v[i]
+    Jv = torch.matmul(grad.view(-1,1).T, v.view(-1,1)).reshape(-1)
+    # Compute the gradient of Jv with respect to y
+    Jv_grad = torch.autograd.grad(outputs=Jv.sum(), inputs=y,retain_graph=True)
+    Jv_grad = torch.cat([x.flatten() for x in Jv_grad], dim=0)
+
+    return Jv_grad
+
+def compute_phi_trace(model, xx, yy, x,y, learning_rate, epochs,train_criterion,device = 'cuda',seed = 1):
+    setup_seed(seed)
+    x_i,y_i,x,y = xx.to(device),yy.to(device),x.to(device),y.to(device)
+    window_length = x_i.shape[1]
+    phi_trace = 0.0
+
+    loss_function = torch.nn.MSELoss()
+
+    optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+
+    with trange(epochs) as t:
+        for i in t:
+            y_i_pred = model(x_i).reshape(-1)
+    
+            loss_i = loss_function(y_i_pred, y_i.reshape(-1))
+
+            local_importance = ntk(model, x_i,y_i, x,y)
+    
+            global_importance = my_get_yh_v_product(model, x_i,y_i,local_importance.reshape(-1),loss_fn=train_criterion)
+
+                        
+            phi_trace -= learning_rate * global_importance
+        
+    
+            optimizer.zero_grad()
+            loss_i.backward()
+            optimizer.step()
+            t.set_description(f'Training (loss: {loss_i.item():.4f})')
+    phi_trace = phi_trace.cpu().detach().numpy().reshape(-1,window_length)
+    return phi_trace,model
+
+def create_directory_if_not_exists(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
